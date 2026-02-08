5844a41725c22b67c0a481865f526e4b8cafecd0
Sisilika <97743633+Sisilika@users.noreply.github.com>
2026-02-08 20:30:49 +0530

    llm.py

--- backend/llm.py
+++ backend/llm.py
@@ -1,44 +1,23 @@
 import requests
-import streamlit as st
 
-OPENROUTER_API_KEY = st.secrets["OPENROUTER_API_KEY"]
-
-import requests
-import streamlit as st
-
-OPENROUTER_API_KEY = st.secrets["OPENROUTER_API_KEY"]
-
-def ask_llm(prompt):
+def ask_llm(prompt, api_key):
 
     response = requests.post(
         "https://openrouter.ai/api/v1/chat/completions",
         headers={
-            "Authorization": f"Bearer {OPENROUTER_API_KEY}",
-            "Content-Type": "application/json",
-            "HTTP-Referer": "https://doc-intelligence-esa5ngepmuwfw6x3ukyytu.streamlit.app",
-            "X-Title": "Doc Intelligence"
+            "Authorization": f"Bearer {api_key}",
+            "Content-Type": "application/json"
         },
         json={
             "model": "mistralai/mixtral-8x7b-instruct",
             "messages": [
-                {
-                    "role": "system",
-                    "content": "You answer using only provided document context."
-                },
-                {
-                    "role": "user",
-                    "content": prompt
-                }
+                {"role": "system", "content": "You answer using only provided document context."},
+                {"role": "user", "content": prompt}
             ]
         }
     )
-
-    print("STATUS:", response.status_code)
-    print("RAW:", response.text)
 
     if response.status_code != 200:
         return response.text
 
     return response.json()["choices"][0]["message"]["content"]
-
-    return resp_json["choices"][0]["message"]["content"]
